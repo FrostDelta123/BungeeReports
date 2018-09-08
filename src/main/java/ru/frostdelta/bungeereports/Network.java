a55c758bbd481f84a3dc6e397018e075c66a1218
FrostDelta123 <pawel.soko2012@yandex.ru>
2018-09-08 18:41:22 +0300

    Network refactor

--- src/main/java/ru/frostdelta/bungeereports/Network.java
+++ src/main/java/ru/frostdelta/bungeereports/Network.java
@@ -1,6 +1,11 @@
 package ru.frostdelta.bungeereports;
 
-import java.sql.*;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
